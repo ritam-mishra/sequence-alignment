d81ac5da3ebebaf0b14f09d1ec45a33cf06338ab
ritam-mishra <ritammishra2002@gmail.com>
2023-11-26 16:06:51 +0000

    modifying algorithms and making them work correctly to the input sequences and generate respective output files.

--- nw1_3.py
+++ nw1_3.py
@@ -1,61 +1,47 @@
 from functions1_3 import nw
 
 # Read alphabet and scores from text file
-f = open("scores.txt", 'r')
-alphabet = f.readline()
-while(alphabet[-1] in ['\n', '\r']):
-    alphabet = alphabet[:-1]
-f.readline()
-gapPenalty = int(f.readline())
-f.readline()
-simMatrix = []
-line = f.readline()
-while(line):
-    row = list(int(x) for x in line.split())
-    simMatrix.append(row)
+with open("scores.txt", 'r') as f:
+    alphabet = f.readline().rstrip('\n\r')
+    f.readline()
+    gapPenalty = int(f.readline())
+    f.readline()
+    simMatrix = []
     line = f.readline()
-f.close()
+    while line:
+        row = list(int(x) for x in line.split())
+        simMatrix.append(row)
+        line = f.readline()
 
 # Create a 1-1 mapping from characters to integers, for simplicity in algorithm
-alphEnum = dict([(alphabet[i], i) for i in range(len(alphabet))])
+alphEnum = {alphabet[i]: i for i in range(len(alphabet))}
 
 # Load input sequences
-f = open("sequences.txt", 'r')
-line = f.readline()
+with open("sequences.txt", 'r') as f:
+    # Open output file, in preparation for storing output alignments
+    with open("alignments_nw.txt", 'w') as g:
+        line = f.readline()
+        while line:
+            # This loop repeats until no more input sequences are found
+            # At each iteration, we read the next two sequences and run the algorithm on them
+            A = line.rstrip('\n\r')
+            B = f.readline().rstrip('\n\r')
+            f.readline()
+            line = f.readline()
 
-# Open output file, in preparation for storing output alignments
-g = open("alignments.txt", 'w')
+            # Run the NW algorithm
+            print("First sequence:", A)
+            print("Second sequence:", B)
+            print("Calculating alignment distance by Needleman-Wunsch method...")
 
-while(line):
-    # This loop repeats until no more input sequences are found
-    # At each iteration we read the next two sequences and run the algorithm on them
-    A = line
-    while(A[-1] in ['\n', '\r']):
-        A = A[:-1]
-    B = f.readline()
-    while(B[-1] in ['\n', '\r']):
-        B = B[:-1]
-    f.readline()
-    line = f.readline()
+            z = nw(A, B, simMatrix, gapPenalty, alphEnum)
 
-    # Run the NW algorithm
+            print("Alignment of A: ", z[0])
+            print("Alignment of B: ", z[1])
+            print("Similarity score: ", z[2], '\n')
 
-    print "First sequence:", A
-    print "Second sequence:", B
-    print "Calculating alignment distance by Needleman-Wunsch method..."
-    
-    z = nw(A, B, simMatrix, gapPenalty, alphEnum)
-
-    print "Alignment of A: ", z[0]
-    print "Alignment of B: ", z[1]
-    print "Similarity score: ", z[2], '\n'
-
-    # Write outputs to text file
-    g.write(str(z[2]) + "\n")
-    g.write(z[0] + "\n")
-    g.write(z[1] + "\n")
-    g.write("\n")
-
-# Close the files and finish
-f.close()
-g.close()
+            # Write outputs to text file
+            g.write(str(z[2]) + "\n")
+            g.write(z[0] + "\n")
+            g.write(z[1] + "\n")
+            g.write("\n")
